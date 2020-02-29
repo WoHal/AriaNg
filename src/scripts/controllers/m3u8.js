14cbb2df806c81fb8632074da44b48c3ef632828
wanghaowh.wang <695877493@qq.com>
2020-02-29 13:19:41 +0800

    update

--- src/scripts/controllers/m3u8.js
+++ src/scripts/controllers/m3u8.js
@@ -3,24 +3,21 @@
 
     angular.module('ariaNg').controller('M3U8Controller', ['$rootScope', '$scope', '$location', '$timeout', 'ariaNgCommonService', 'ariaNgSettingService', 'ariaNgFileService', 'aria2SettingService', 'aria2TaskService', function ($rootScope, $scope, $location, $timeout, ariaNgCommonService, ariaNgSettingService, ariaNgFileService, aria2SettingService, aria2TaskService) {
         $scope.context = {
-            currentTab: 'links',
-            taskType: 'urls',
             urls: '',
-            uploadFile: null,
-            availableOptions: (function () {
-                var keys = aria2SettingService.getNewTaskOptionKeys();
-
-                return aria2SettingService.getSpecifiedOptions(keys, {
-                    disableRequired: true
-                });
-            })(),
             globalOptions: null,
-            options: {},
-            optionFilter: {
-                global: true,
-                http: false,
-                bittorrent: false
+            options: {}
+        };
+
+        $scope.loadDefaultOption = function () {
+            if ($scope.context.globalOptions) {
+                return;
             }
+
+            $rootScope.loadPromise = aria2SettingService.getGlobalOption(function (response) {
+                if (response.success) {
+                    $scope.context.globalOptions = response.data;
+                }
+            });
         };
 
         $scope.startM3U8Download = function () {
